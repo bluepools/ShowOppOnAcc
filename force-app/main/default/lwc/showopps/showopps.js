e663f3051d2080be97882ca827a4302d7fb4ade1
Olexandr <thedoctor7342@gmail.com>
2022-05-06 12:05:52 +0300

    first commit

---
+++ force-app/main/default/lwc/showopps/showopps.js
@@ -0,0 +1,65 @@
+import { LightningElement, wire, track } from 'lwc';
+
+import getOpportunity from '@salesforce/apex/Spring2022LWCtestController.getOpportunity';
+
+export default class Spring2022LWCtest extends LightningElement {
+
+    contlist = [];
+    columns = [{
+        label: 'Name',    
+        fieldName: 'FirstNameUrl',
+        type: 'url',
+        typeAttributes: {label: { fieldName: 'Name' }, target: '_blank'},
+        sortable: "true"
+    },
+    {
+        label: 'Stage',
+        fieldName: 'StageName',
+        sortable: "true"
+    },
+    {
+        label: 'Close Date',
+        fieldName: 'CloseDate',
+        sortable: "true",
+        type: "date"
+    
+    },
+    {
+        label: 'Amount',
+        fieldName: 'Amount',
+        sortable: "true",
+        type: 'currency',
+        cellAttributes: { alignment: 'left' },
+    } ];
+
+    @wire(getOpportunity)
+    
+    opportunitys({ error, data }) {
+
+        if (data) {
+            let tempConList = []; 
+            
+            data.forEach((record) => {
+                let tempConRec = Object.assign({}, record);  
+                tempConRec.FirstNameUrl = '/' + tempConRec.Id;
+                tempConList.push(tempConRec);
+ 
+            });
+            
+            this.contlist = tempConList;
+            this.error = undefined;
+
+            console.table(this.contlist);
+
+        } else if (error) {
+            this.error = result.error;
+        }
+    }
+    
+   
+}
+
+    
+
+    
+
